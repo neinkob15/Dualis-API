3f2683ad1a0e95e6aee62f92a7256cc915c1f4b8
Jakob Gietl <jakob-gietl@gmx.de>
2020-05-22 17:23:35 +0200

    new method of comparison -> using more jq now

--- notenCompare.sh
+++ notenCompare.sh
@@ -13,41 +13,10 @@ fi
 
 /bin/bash /opt/dualis-app/NOTEN.sh -u $username -p $password > /opt/dualis-app/noten.log
 
-readarray -t a < /opt/dualis-app/noten.log
-readarray -t b < /opt/dualis-app/noten2.log
+serialized1=$(cat noten.log | jq -r '.[] | .modules[] as $m | $m.exams[] as $e | "\($e.grade) => \($e.exam) [\($m.name)]"')
+serialized2=$(cat noten2.log | jq -r '.[] | .modules[] as $m | $m.exams[] as $e | "\($e.grade) => \($e.exam) [\($m.name)]"')
 
-c=0
-
-for i in "${a[@]}"
-do
-	if [[ "$i" == *"\"name\""* ]];then
-		module=$(echo "$i" | sed 's/ *\"name\":\"\([^\"]*\)\".*/\1/g')
-	fi
-	if [[ "$i" == *"\"exam\""* ]];then
-		exam=$(echo "$i" | sed 's/ *\"exam\":\"\([^\"]*\)\".*/\1/g')
-	fi
-	if ! [[ "$i" == "${b[$c]}" ]];then
-
-		if ! [[ "$i" == *"{"* || "$i" == *"}"* || "$i" == *"]"* || "$i" == *"["* ]];then
-			if [[ "$i" == *"\"grade\""* ]];then
-				grade=$(echo "$i" | sed 's/ *\"grade\":\"\([^\"]*\)\".*/\1/g')
-				if ! [[ "$grade" == "-" ]];then
-					mailText=$(printf "%s\n%s => %s [%s]" "$mailText" "$grade" "$exam" "$module")
-				fi
-			fi
-		fi
-
-		f=$(echo "${b[$c]}" | sed 's/^ *//g' | head -c 5)
-		g=$(echo "$i" | sed 's/^ *//g' | head -c 5)
-	        if [[ "$g" == "$f" && "$i" == *"\"grade\""* ]];then
-                	c=$((c+1))
-	        fi
-	else
-		c=$((c+1))
-	fi
-done
-
-mailText=$(echo "$mailText" | tail -n +2)
+mailText=$(diff --changed-group-format="%<" --unchanged-group-format="" <(echo "$serialized1") <(echo "$serialized2"))
 
 if [ -z "$mailText" ];then
 	echo "Keine neuen Noten."
