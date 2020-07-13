8f24a935c9ffbf99544fdb5189b9f107e8699b14
Jakob Gietl <jakob-gietl@gmx.de>
2020-07-13 20:38:38 +0200

    removed message when new grade is -

--- notenCompare.sh
+++ notenCompare.sh
@@ -16,9 +16,8 @@ fi
 serialized1=$(cat /opt/dualis-app/noten.log | /usr/bin/jq -r '.[] | .modules[] as $m | $m.exams[] as $e | "\($e.grade) => \($e.exam) [\($m.name)]"')
 serialized2=$(cat /opt/dualis-app/noten2.log | /usr/bin/jq -r '.[] | .modules[] as $m | $m.exams[] as $e | "\($e.grade) => \($e.exam) [\($m.name)]"')
 
-mailText=$(/usr/bin/diff --changed-group-format="%<" --unchanged-group-format="" <(echo "$serialized1") <(echo "$serialized2"))
+mailText=$(/usr/bin/diff --changed-group-format="%<" --unchanged-group-format="" <(echo "$serialized1") <(echo "$serialized2") | grep "^[^-]")
 
-echo "$mailText" > t
 
 if [ -z "$mailText" ];then
 	echo "Keine neuen Noten."
@@ -27,7 +26,7 @@ if [ -z "$mailText" ];then
 fi
 
 /usr/bin/mail -s 'Neue Noten!' -a From:NotenAdmin\<jakob@neinkob.de\> jakob-gietl@gmx.de <<< $(echo "$mailText")
-mailText2=$(echo "$mailText" | sed -z 's/\n/%0A/g')
+mailText2=$(echo -e "$mailText")
 /usr/bin/curl -s -g -X POST "https://api.telegram.org/bot$bot/sendMessage?chat_id=$chatid" --data-urlencode "text=$mailText2" > /dev/null
 
 echo "$mailText"
